0f42739e2a76314106162a04043698d3673fbf61
Phil White <philipcwhite@users.noreply.github.com>
2021-08-24 00:41:42 +0000

    Create metric.py
    This file uses the OpenTelemetry SDK to create three files for testing.  A JSON file, a protobuf file (.pb), and a decoded protobuf file.

---
+++ install/test/metric.py
@@ -0,0 +1,44 @@
+from google.protobuf.json_format import MessageToJson
+from opentelemetry.proto.metrics.v1.metrics_pb2 import Metric, ResourceMetrics, InstrumentationLibraryMetrics, NumberDataPoint
+from opentelemetry.proto.common.v1.common_pb2 import KeyValue
+from opentelemetry.proto.collector.metrics.v1.metrics_service_pb2 import ExportMetricsServiceRequest
+ 
+ 
+# Instantiate Classes
+my_exportmetricsservicerequest = ExportMetricsServiceRequest()
+my_resourcemetrics = ResourceMetrics()
+my_instrumentationlibrarymetrics = InstrumentationLibraryMetrics()
+my_metric = Metric()
+my_number = NumberDataPoint()
+my_keyvalue = KeyValue()
+ 
+ 
+# Create Metric
+my_metric.name = 'otel.cpu.percent'
+my_metric.description = 'CPU Percent'
+my_metric.unit = '%'
+my_metric.sum.aggregation_temporality = 2 # Cumulative
+my_number.time_unix_nano = 1626970731000000000
+my_number.as_int = 1234
+ 
+ 
+# Create Resource Tag
+my_keyvalue.key = 'host.name'
+my_keyvalue.value.string_value = 'white05'
+ 
+ 
+# Create packet
+my_metric.sum.data_points.extend([my_number])
+my_instrumentationlibrarymetrics.metrics.extend([my_metric])
+my_resourcemetrics.resource.attributes.extend([my_keyvalue])
+my_resourcemetrics.instrumentation_library_metrics.extend([my_instrumentationlibrarymetrics])
+my_exportmetricsservicerequest.resource_metrics.extend([my_resourcemetrics])
+ 
+ 
+# Write packets to files
+with open("metric.pb", "wb") as f:
+    f.write(my_exportmetricsservicerequest.SerializeToString())
+with open("metric.protobuf", "w") as f:
+    f.write(str(my_exportmetricsservicerequest))
+with open("metric.json", "w") as f:
+    f.write(str(MessageToJson(my_exportmetricsservicerequest)))
