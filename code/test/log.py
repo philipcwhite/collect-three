85f700928c1d0c7b6485c101df6b9504bc074ac9
Phil White <philipcwhite@users.noreply.github.com>
2022-03-23 14:29:11 -0400

    Update log.py

--- code/test/log.py
+++ code/test/log.py
@@ -1,2 +1,2 @@
 from recorder import log
-log.record("test_log", "Info", "Body message", {"log_attribute":"test"}, {"resource_attribute":"test"})
+log.record("Info", "Body message", {"log_attribute":"test"}, {"resource_attribute":"test"})
