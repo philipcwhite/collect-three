9d4fef609223b8d0dbdd79f83d65f0d0faa70686
Phil White <philipcwhite@users.noreply.github.com>
2022-04-13 15:52:54 -0400

    Update trace.py

--- code/test/trace.py
+++ code/test/trace.py
@@ -1,3 +1,4 @@
 import time
-from recorder import trace
-trace.record('test', int(time.time()*1000000000), int(time.time()*1000000000), {'hello':'world','foo':'bar'}, {'hello':'world','foo':'bar'})
+from recorder import Trace
+trace = Trace('test', int(time.time()*1000000000), int(time.time()*1000000000), {'hello':'world','foo':'bar'}, {'hello':'world','foo':'bar'})
+trace.record()
